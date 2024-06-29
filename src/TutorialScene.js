58680a43bace02ea587a8793d5fc77f7088e10b7
Nellingten <nhuebner@stud.hs-bremen.de>
2024-06-29 23:42:37 +0200

    map nicht zusehen
    mp kann man noch nicht sehen weil Viewport nicht gesettet wird

--- src/TutorialScene.js
+++ src/TutorialScene.js
@@ -4,21 +4,33 @@ export default class TutorialScene extends Phaser.Scene {
   }
   
   create() {
-        
-    const levelConfig = this.registry.get('level1Config'); // Zugriff auf die gespeicherte Level-Konfiguration
-      // Erstellt die Textbox an den Koordinaten (100, 100)
-      const introText = this.add.text(200, 100, 'Willkommen in Cubworld!', { fontSize: '20px', fill: '#fff', backgroundColor: '#cccccc', padding: { x: 10, y: 5 } });
+    const levelConfig = this.registry.get('level1Config');
 
-     
-      // Zugriff auf die gespeicherte Karte
-    const mapData = this.registry.get('mapData');
-    const map = this.make.tilemap({ key: levelConfig.tileMap});
+    // Textbox
+    const introText = this.add.text(200, 100, 'Willkommen in Cubworld!', {
+      fontSize: '20px',
+      fill: '#fff',
+      backgroundColor: '#cccccc',
+      padding: { x: 10, y: 5 }
+    });
+
+    // Karte und Tileset
+    const map = this.make.tilemap({ key: levelConfig.tileMap });
     const tileset = map.addTilesetImage('CanGo', 'canGoImage');
     const mapLayer = map.createLayer('CanGo', tileset, 0, 0);
 
-    const levelTexture = this.add.image(400, 300, 'level1Texture'); // Adjust position as needed
-levelTexture.setDepth(1); // Ensure it's above the tile layer
+    // Level-Textur
+    const levelTexture = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'map1');
 
+    // die machen immoment alles kaputt wegen dem kack resize funkctions ughhhh
+    levelTexture.scaleX = 1 / levelConfig.mapTileSize[0] * 64;
+    levelTexture.x = this.WIDTH / 2 + levelConfig.mapPositionOffset[0];
+    levelTexture.scaleY = 1 / levelConfig.mapTileSize[0] * 64;
+    levelTexture.y = this.HEIGHT / 2 + levelConfig.mapPositionOffset[1];
+    
+    
   }
+
+ 
+   
 }
-  
