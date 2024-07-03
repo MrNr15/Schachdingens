b47e6bcdf4d770c0c9bda29ab24217857ab24c62
Nellingten <nhuebner@stud.hs-bremen.de>
2024-07-03 18:13:12 +0200

    gameover button gefixed

--- src/game.js
+++ src/game.js
@@ -265,8 +265,6 @@ export default class Game extends Phaser.Scene {
 
     setupFigures() {
         this.player = new player(this, this.levelConfig.player[0], this.levelConfig.player[1]);
-        this.player.setDepth(0); // Beispiel-Depth für den Spieler
-
         for (var i = 0; i < this.levelConfig.enemys.length; i++) {
             let e;
             let type = this.levelConfig.enemyTypes[i];
@@ -281,7 +279,6 @@ export default class Game extends Phaser.Scene {
                     e = new Enemy3(this, this.levelConfig.enemys[i][0], this.levelConfig.enemys[i][1]);
                     break;
             }
-            e.setDepth(0); // Beispiel-Depth für Gegner
             this.enemys.push(e);
             
         }
@@ -475,9 +472,9 @@ export default class Game extends Phaser.Scene {
         // const gameOver = this.add.text(this.WIDTH / 2, this.HEIGHT / 2, 'Game over', { fill: '#000' });
         // const playAgain = this.add.text(this.WIDTH / 2, this.HEIGHT / 2 + 100, 'Play again', { fill: '#000' });
         const gameOver  = this.add.image(this.cameras.main.width - 690, this.cameras.main.height - 550, 'GameOver');
-        gameOver.setDepth(10);
+        gameOver.setDepth(this.y+1);
         const playAgain  = this.add.image(this.cameras.main.width - 690, this.cameras.main.height - 350, 'PlayAgain');
-        playAgain.setDepth(10);
+        playAgain.setDepth(this.y+1);
 
         playAgain.setInteractive();
         playAgain.on('pointerdown', () => {
